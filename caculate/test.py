cef5ab3d84f77604ca10ba54c26b7c71e9085894
wang12346 <84620020+wang12346@users.noreply.github.com>
2022-04-01 14:11:37 +0800

    Update test.py

--- caculate/test.py
+++ caculate/test.py
@@ -4,9 +4,25 @@ import random
 from unittest import TestCase, main
 from caculator import caculator as cl
 import math
+import numpy as np
 
 
 test_number = 1000#测试次数
+
+list = []#随机输入
+
+        for i in range(32767):
+            list.append(i)
+        list.append('+')
+        list.append('-')
+        list.append('*')
+        list.append('/')
+        list.append('!')
+        list.append('@')
+        list.append('#')
+        list.append('$')
+        list.append('%')
+        list.append('^')
 
 class MyTestCase(TestCase):
 
@@ -130,7 +146,7 @@ class MyTestCase(TestCase):
             string1 = list(string1)
             string1[1] = x
             equation1 = " ".join(map(str, string1))
-            result_calculator = cl(equation1)
+            result_calculator = round(cl(equation1), 4)
 
             if result_calculator == "出现错误，请重新输入！":
                 print('x = ', x)
@@ -138,7 +154,7 @@ class MyTestCase(TestCase):
             else:
                 x = math.radians(x)
                 result_true = math.tan(x)
-                result_true = round(result_true, 8)
+                result_true = round(result_true, 4)
                 print('x = ', x)
                 print('result_calculator = ', result_calculator)
                 print('result_true = ', result_true)
@@ -159,10 +175,10 @@ class MyTestCase(TestCase):
             string1[2] = x * 180
             equation1 = " ".join(map(str, string1))
 
-            result_calculator = cl(equation1)
+            result_calculator = round(cl(equation1), 4)
             if (x % 0.5 != 0):
                 result_true = math.tan(x * math.pi)
-                result_true = round(result_true, 8)
+                result_true = round(result_true, 4)
             else:
                 result_true = "出现错误，请重新输入！"
             print('x = ', x)
@@ -172,37 +188,81 @@ class MyTestCase(TestCase):
             self.assertEqual(result_calculator, result_true, "tan弧度功能，执行结果不正确")
     def test_arcsin(self):  # 测试arcsin
 
-        # 测试角度
         for i in range(test_number):
-            # x = random.uniform(0,1)
-            x = 0.5
+            x = random.uniform(0,1)
             string1 = '$x'
             string1 = list(string1)
             string1[1] = x
             equation1 = " ".join(map(str, string1))
-            result_calculator = cl(equation1)
-            x = math.radians(x)
-            result_true = math.asin(x)
-            result_true = round(result_true, 8)
+
+            result_calculator = round(cl(equation1), 4)
+
+
+            result_true = np.arcsin(x)
+            result_true = np.degrees(result_true)
+            result_true = round(result_true, 4)
+
             print('x = ', x)
             print('result_calculator = ', result_calculator)
             print('result_true = ', result_true)
 
-            self.assertEqual(result_calculator, result_true, "arcsin角度功能，执行结果不正确")
+            self.assertEqual(result_calculator, result_true, "arcsin功能，执行结果不正确")
 
-            string1 = '$(x)'
+    def test_arccos(self):  # 测试arccos
+
+        for i in range(test_number):
+            x = random.uniform(0,1)
+            string1 = '%x'
             string1 = list(string1)
-            string1[2] = x * 180
+            string1[1] = x
             equation1 = " ".join(map(str, string1))
 
-            result_calculator = cl(equation1)
-            result_true = math.asin(x * math.pi)
-            result_true = round(result_true, 8)
+            result_calculator = round(cl(equation1), 4)
+
+
+            result_true = np.arccos(x)
+            result_true = np.degrees(result_true)
+            result_true = round(result_true, 4)
+
             print('x = ', x)
             print('result_calculator = ', result_calculator)
             print('result_true = ', result_true)
 
-            self.assertEqual(result_calculator, result_true, "arcsin弧度功能，执行结果不正确")
+            self.assertEqual(result_calculator, result_true, "arccos功能，执行结果不正确")
+
+    def test_arctan(self):  # 测试arccos
+
+        for i in range(test_number):
+            x = random.uniform(0,32767)
+            string1 = '^x'
+            string1 = list(string1)
+            string1[1] = x
+            equation1 = " ".join(map(str, string1))
+
+            result_calculator = round(cl(equation1), 4)
+
+
+            result_true = np.arctan(x)
+            result_true = np.degrees(result_true)
+            result_true = round(result_true, 4)
+
+            print('x = ', x)
+            print('result_calculator = ', result_calculator)
+            print('result_true = ', result_true)
+
+            self.assertEqual(result_calculator, result_true, "arctan功能，执行结果不正确")
+
+    def test_error(self):
+
+        test_list = []
+        for i in range(test_number):
+            for j in range(random.randint(0,20)):
+                test_list.append(list[random.randint(0,23767)])
+
+
+
+
+
 
 
 
